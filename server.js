b9ec85317bab77a6f6404c6c9a87d35935c0a354
jstearne <jared.stearne@gmail.com>
2021-12-31 14:29:28 -0800

    Got API objects but limited 10/day. Seasons listed with seven to choose from.

--- server.js
+++ server.js
@@ -10,33 +10,13 @@ const app = express();
 const morgan = require('morgan');
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs');
-
-// routes
-app.get('/', (req, res) => {
-    res.render('index');
-    }
-);
-
-app.get('/hello', function(request, response){
-    response.send("Hello Friends!");
-});
 
 
-app.use("/api", routes);
-
-//This is to catch anything that's trying to hit an api route that isn't made
-app.all("/api/*", function (req, res, next) {
-    res.send("THIS IS NOT AN API ROUTE");
-});
-
-
-// API
 var axios = require("axios").default;
 
 var options = {
   method: 'GET',
-  url: 'https://api-nba-v1.p.rapidapi.com/teams/teamId/%7Bteamid%7D',
+  url: 'https://api-nba-v1.p.rapidapi.com/seasons/',
   headers: {
     'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
     'x-rapidapi-key': '996a75504dmsh27d601a97da1825p164667jsn4f11b461ba50'
@@ -47,6 +27,19 @@ axios.request(options).then(function (response) {
 	console.log(response.data);
 }).catch(function (error) {
 	console.error(error);
+});
+
+
+// view engine
+app.set('view engine', 'ejs');
+
+// routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/hello', function(request, response){
+    response.send("Hello Friends!");
 });
 
 // app listener
